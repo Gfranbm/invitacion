f788882795c83d54fbfe23418428cc173642e92f
Gbritoappsystems <g.brito@sistemas.com.gt>
2023-09-21 12:10:05 -0600

    se elimina cache

--- index.php
+++ index.php
@@ -14,6 +14,9 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv='cache-control' content='no-cache'>
+    <meta http-equiv='expires' content='0'>
+    <meta http-equiv='pragma' content='no-cache'>
     <link
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"
